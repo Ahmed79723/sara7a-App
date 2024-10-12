e098307cff457ec2b9113a1dd0636297a0aff9f6
Ahmed79723 <ashrafahmed797@gmail.com>
2024-10-12 17:39:21 +0300

    10th commit

--- dataBase/dbConnection.js
+++ dataBase/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = mongoose
   .connect(
-    `mongodb+srv://ahmed:black24mamba@e-commerce.j07rt.mongodb.net/sara7a_APP`,
+    `mongodb+srv://${process.env.cloud_DB_user}:${process.env.cloud_DB_pass}@e-commerce.j07rt.mongodb.net/sara7a_APP`,
     {
       serverSelectionTimeoutMS: 5000,
     }
