d725cd5f0014ddae12124bceb5a10c37e039ed0d
Ahmed79723 <ashrafahmed797@gmail.com>
2024-10-12 16:26:52 +0300

    db

--- dataBase/dbConnection.js
+++ dataBase/dbConnection.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 export const dbConnection = mongoose
   .connect(`mongodb://127.0.0.1:27017/sarahah_App`, {
     serverSelectionTimeoutMS: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("database connected successfully!");
